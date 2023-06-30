700a04b94ddc6b10510a9e8f5dc33ec230fa2b9f
David Gomes <LegendL3n@users.noreply.github.com>
2023-06-30 15:19:52 +0100

    refactor: decouple the boot requirements from the base app widget

---
+++ lib/ui/screen/saved_texts_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class SavedTextsScreen extends StatelessWidget {
+  const SavedTextsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
