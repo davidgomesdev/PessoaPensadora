8313513d797929cb42377d9d03cd4534c799ee46
David Gomes <LegendL3n@users.noreply.github.com>
2023-07-23 12:56:24 +0100

    chore: remove leftover

--- lib/model/bloc/selection_bloc.dart
+++
@@ -1,7 +0,0 @@
-import 'package:pessoa_bonito/model/pessoa_category.dart';
-
-class CategorySelectionState {
-  final PessoaCategory? currentCategory, previousCategory;
-
-  CategorySelectionState({this.currentCategory, this.previousCategory});
-}
