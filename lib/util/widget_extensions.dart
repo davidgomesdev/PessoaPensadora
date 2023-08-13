fd71413a56b725bbb73916c98ff0383c25f32dec
David Gomes <LegendL3n@users.noreply.github.com>
2023-08-13 16:37:12 +0100

    feat: add "define" button in text reader word selection

--- lib/util/widget_extensions.dart
+++ lib/util/widget_extensions.dart
@@ -2,6 +2,11 @@ import 'package:flutter/widgets.dart';
 
 extension EditableTextStateExt on EditableTextState {
   String getSelectedText() {
+    final isSelectionEmpty = textEditingValue.selection.start == -1 &&
+        textEditingValue.selection.end == -1;
+
+    if (isSelectionEmpty) return '';
+
     return textEditingValue.selection.textInside(textEditingValue.text);
   }
 }
