f458368f967d610c959fc9884590a656a6359362
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-04-05 18:45:41 +0100

    feat: add share button on readers

---
+++ lib/ui/widget/share_text_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pessoa_bonito/service/selection_action_service.dart';
+
+class ShareTextButton extends StatelessWidget {
+  final String text, author;
+
+  const ShareTextButton({super.key, required this.text, required this.author});
+
+  @override
+  Widget build(BuildContext context) {
+    final SelectionActionService service = Get.find();
+
+    return IconButton(
+      onPressed: () {
+        service.shareText(text, author);
+      },
+      icon: const Icon(Icons.ios_share_rounded),
+    );
+  }
+}
