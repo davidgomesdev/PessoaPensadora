eba0df52c8c5e53f1f60432734efc814ccaae424
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-04-20 18:55:35 +0100

    fix: text filter not working

--- test/boot_service_test.dart
+++ test/boot_service_test.dart
@@ -2,6 +2,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:pessoa_bonito/service/text_store.dart';
@@ -16,7 +17,7 @@ void main() {
     exampleJson = File('test/assets/example.json').readAsStringSync();
   });
 
-  test('TextStoreService reads texts json properly', () async {
+  test('TextStoreService reads sample texts json properly', () async {
     final assetBundleMock = MockAssetBundle();
 
     when(assetBundleMock.loadString(any)).thenAnswer((_) async => exampleJson);
@@ -54,5 +55,30 @@ void main() {
     expect(categoryWithSubcategoriesAndTexts.texts, hasLength(2));
     expect(categoryWithSubcategoriesAndTexts.texts.first.id, equals(1234));
     expect(categoryWithSubcategoriesAndTexts.texts[1].id, equals(9));
+  });
+
+  test('TextStoreService reads real texts json and functions properly', () async {
+    final realJson = File('assets/json_files/texts.json').readAsStringSync();
+
+    final assetBundleMock = MockAssetBundle();
+
+    when(assetBundleMock.loadString(any)).thenAnswer((_) async => realJson);
+
+    final service = await TextStoreService.initialize(assetBundleMock);
+
+    Get.put(service);
+
+    final index = service.index;
+
+    expect(index.subcategories, hasLength(9));
+    expect(index.texts, isEmpty);
+
+    final marPortuguesCategory = service.getCategory(34);
+
+    expect(marPortuguesCategory.title, "Segunda parte: MAR PORTUGUÊS");
+
+    final infanteRootCategory = service.getTextRootCategory(2375);
+
+    expect(infanteRootCategory.title, "Poesia Ortónima de Fernando Pessoa");
   });
 }
