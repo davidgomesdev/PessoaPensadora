ec61a21cdeb436434dfb85ddd7dc0444117e9934
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-04-25 09:42:32 +0000

    test: add saved text tests

--- test/feature_saved_test.dart
+++ test/feature_saved_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pessoa_bonito/ui/widget/text_reader.dart';
 
 import 'utils.dart';
 
@@ -34,9 +35,9 @@ void main() {
     await tester.tap(find.byIcon(Icons.bookmark_outline_outlined));
     await tester.pumpAndSettle();
 
-    await openDrawer(tester);
-
     expect(find.byIcon(Icons.bookmark_outlined), findsOne);
+
+    await openDrawer(tester);
 
     await tester.tap(find.byIcon(Icons.bookmarks));
     await tester.pumpAndSettle();
@@ -44,7 +45,7 @@ void main() {
     expect(
         find.descendant(
             of: find.widgetWithText(ExpansionTile, 'Rubaiyat'),
-            matching: find.text('A vida é terra e o vivê-la é lodo.')),
+            matching: textFinder),
         findsOne);
   });
 
@@ -65,15 +66,15 @@ void main() {
     await tester.tap(find.byIcon(Icons.bookmark_outline_outlined));
     await tester.pumpAndSettle();
 
-    await openDrawer(tester);
-
     expect(find.byIcon(Icons.bookmark_outlined), findsOne);
+
+    await openDrawer(tester);
 
     await tester.tap(find.byIcon(Icons.bookmarks));
     await tester.pumpAndSettle();
 
     final tileFinder = find.ancestor(
-        of: find.text('A vida é terra e o vivê-la é lodo.'),
+        of: textFinder,
         matching: find.byType(Slidable));
 
     await tester.drag(tileFinder, const Offset(-500.0, 0));
@@ -95,5 +96,48 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.byIcon(Icons.bookmark_outline_outlined), findsOne);
+
+    await openDrawer(tester);
+
+    await tester.tap(find.byIcon(Icons.bookmarks));
+    await tester.pumpAndSettle();
+
+    expect(tileFinder, findsNothing);
   });
+
+  testWidgets('Clicking on a saved text should make the saved reader screen show with it',
+      (tester) async {
+    await startApp(tester);
+    await openDrawer(tester);
+
+    await tester.tap(find.text('Rubaiyat'));
+    await tester.pumpAndSettle();
+
+    var textFinder = find.text('A vida é terra e o vivê-la é lodo.');
+
+    await tester.tap(textFinder);
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byIcon(Icons.bookmark_outline_outlined));
+    await tester.pumpAndSettle();
+
+    await openDrawer(tester);
+
+    await tester.tap(find.byIcon(Icons.bookmarks));
+    await tester.pumpAndSettle();
+
+    await tester.tap(textFinder);
+    await tester.pumpAndSettle();
+    
+    final reader = tester.widget<TextReader>(find.byType(TextReader));
+
+    expect(reader.author, equals('Fernando Pessoa'));
+    expect(reader.categoryTitle, equals('Rubaiyat'));
+    expect(reader.title, equals('A vida é terra e o vivê-la é lodo.'));
+    expect(reader.content, contains('Em tudo quanto faças sê só tu'));
+    
+    expect(textFinder, findsOne);
+    expect(find.textContaining('Em tudo quanto faças sê só tu'), findsOne);
+  });
+
 }
