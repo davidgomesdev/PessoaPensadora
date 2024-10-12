0c87ddc191b6f083049fa85dadea15f2727d77f0
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-10-12 18:40:26 +0100

    test: add reading type test on history screen

--- test/feature_history_test.dart
+++ test/feature_history_test.dart
@@ -6,8 +6,9 @@ import 'utils.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Click on a text in the drawer should make it appear in the history',
-          (tester) async {
+  testWidgets(
+      'Click on a text in the drawer should make it appear in the history',
+      (tester) async {
     await startApp(tester);
     await openDrawer(tester);
 
@@ -31,6 +32,56 @@ void main() {
     await tester.tap(find.byIcon(Icons.history));
     await tester.pumpAndSettle();
 
-    expect(find.descendant(of: find.byType(ListTile), matching: textFinder), findsOne);
+    expect(find.descendant(of: find.byType(ListTile), matching: textFinder),
+        findsOne);
+  });
+
+  testWidgets(
+      'When switching to reading type main after clicking on a text in the drawer, the text should appear in the history',
+      (tester) async {
+    await startApp(tester);
+    await openDrawer(tester);
+
+    await tester.tap(find.byIcon(Icons.history));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(ListTile), findsNothing);
+
+    await tester.pageBack();
+    await tester.pumpAndSettle();
+
+    await openDrawer(tester);
+
+    await switchReadingTypeToFull(tester);
+
+    final rootCategoryFinder =
+        find.widgetWithText(ListTile, "Textos Filosóficos");
+
+    await dragDrawerUntilVisible(tester, rootCategoryFinder);
+    await tester.pumpAndSettle();
+
+    await tester.tap(rootCategoryFinder);
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Filósofos'));
+    await tester.pumpAndSettle();
+
+    final textFinder = find.text('CHANGE');
+
+    await tester.tap(textFinder);
+    await tester.pumpAndSettle();
+
+    await openDrawer(tester);
+
+    await hitBackDrawerButton(tester);
+    await hitBackDrawerButton(tester);
+
+    await switchReadingTypeToMain(tester);
+
+    await tester.tap(find.byIcon(Icons.history));
+    await tester.pumpAndSettle();
+
+    expect(find.descendant(of: find.byType(ListTile), matching: textFinder),
+        findsOne);
   });
 }
