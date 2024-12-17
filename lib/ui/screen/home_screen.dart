1a296f7a3c297d0ae89f6fafa6c299f2e842ae24
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-12-17 19:50:04 +0000

    chore: bump versions

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pessoa_pensadora/model/pessoa_text.dart';
